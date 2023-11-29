bb7afad49ab103bb505111e31fa34d11ffae5542
Oscar <oscar.munoz103@alu.ulpgc.es>
2023-11-29 16:50:31 +0000

    Variable name changed in Main

--- src/main/java/software/ulpgc/kata3/Main.java
+++ src/main/java/software/ulpgc/kata3/Main.java
@@ -3,16 +3,14 @@ package software.ulpgc.kata3;
 import software.ulpgc.kata2.CsvFileWaterLoader;
 import software.ulpgc.kata2.Water;
 
-import java.awt.*;
 import java.io.File;
 import java.util.List;
-import software.ulpgc.kata2.*;
 
 public class Main {
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
         List<Water> data = new CsvFileWaterLoader(new File("water_potability.csv")).load();
-        double[] finalData = data.stream().mapToDouble(Main::waterToDouble).toArray();
+        double[] finalData = data.stream().mapToDouble(Main::waterHardnessToDouble).toArray();
         mainFrame.getHistogramDisplay().show("HARDNESS HISTOGRAM",
                 "Hardness",
                 "Frequency",
@@ -25,7 +23,7 @@ public class Main {
 
     }
 
-    private static double waterToDouble(Water water) {
+    private static double waterHardnessToDouble(Water water) {
         return water.getHardness();
     }
 
